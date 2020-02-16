eb5fcf0a7cba073065a3581a784e6d7f0d936c55
Guilherme Abreu <sabreuguilherme@gmail.com>
2020-02-16 10:56:58 -0300

    Atualizando com autentica§ão

--- src/main/java/br/com/abreu/sptrans/api/business/impl/BusInformartionBusinessImpl.java
+++ src/main/java/br/com/abreu/sptrans/api/business/impl/BusInformartionBusinessImpl.java
@@ -1,13 +1,24 @@
 package br.com.abreu.sptrans.api.business.impl;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import br.com.abreu.sptrans.api.business.BusInformartionBusiness;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 public class BusInformartionBusinessImpl implements BusInformartionBusiness {
+
+	@Value("${sptrans-uri}")
+	private String URI_SPTRANS;
+
+	@Value("${sptrans-token}")
+	private String TOKEN;
 
 	private final RestTemplate restTemplate;
 
@@ -17,9 +28,21 @@ public class BusInformartionBusinessImpl implements BusInformartionBusiness {
 
 	@Override
 	public String getBusLine(String termoBusca) {
-		return restTemplate.getForObject("http://api.olhovivo.sptrans.com.br/v2.1/Linha/Buscar?termosBusca=8000",
-				String.class);
+		this.authenticate();
+		return restTemplate.getForObject(URI_SPTRANS + "Linha/Buscar?termosBusca=8000", String.class);
 
+	}
+
+	private Boolean authenticate() {
+		log.info("Autenticando");
+		ResponseEntity<String> response = restTemplate.postForEntity(
+				URI_SPTRANS + "Login/Autenticar?token=9dd7814d1c159b7df312a3a9022c0700903fdaeae3ca6a2350998cb93e25962f",
+				null, String.class);
+
+		if (response.getStatusCode().equals(HttpStatus.OK)) {
+			return Boolean.valueOf(response.getBody());
+		}
+		return false;
 	}
 
 }
